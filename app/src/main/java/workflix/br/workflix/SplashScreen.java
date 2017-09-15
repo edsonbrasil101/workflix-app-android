dd7fa9749d82f0b778b9b49942b37885bc2e3856
marcelodanielnunesbrasil <marcelodaniel.daniel@gmail.com>
2017-09-15 00:05:23 -0300

    initial commit

---
+++ app/src/main/java/workflix/br/workflix/SplashScreen.java
@@ -0,0 +1,24 @@
+package workflix.br.workflix;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2500);
+    }
+}
